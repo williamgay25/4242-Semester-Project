548ea0af23dbc0a75e3819e3987bd1db71d32e63
William Gay <williamgay360@gmail.com>
2023-04-20 13:51:27 -0400

    feat: resume return is done

--- server/api/cluster.py
+++ server/api/cluster.py
@@ -1,34 +1,36 @@
 import os
-import time
+from sklearn.cluster import OPTICS
 import numpy as np
 import pandas as pd
-from sklearn.cluster import OPTICS
+import time
 
-def cluster(seed_song = None, artist = None, year = None, length = 2000):
+
+def cluster(seed_song = None, artist = None, year = None, length = 35):
     data_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data.csv')
     big_data = pd.read_csv(data_file_path, index_col=0)
     data = big_data.sample(frac = 0.02).reset_index(drop=True)
-
-    print("Big data index: ", big_data.index)
-    print("Data index: ", data.index)
     
     #Inputs here Please
-    inputs = {"Seed Song": seed_song, "Artist": artist, "Year": year, "Length": length}
+    inputs = {"Seed Song": seed_song, "Artist": artist, "Year": year, "Length": length*60}
     
     artists = []
     decades = []
     
+    seed_existence = False
     #Manipulates data based on presence of a seed song
     if (inputs["Seed Song"] != None):
-        print(data["song_title"])
-        print(inputs["Seed Song"])
-        seed_row = big_data.loc[data["song_title"] == inputs["Seed Song"]]
-        data = data.append(seed_row, big_data = True)
-        artists.append(seed_row["artist_name"].iloc[0])
-        decades.append(seed_row["year"].iloc[0])
-        
-        data["key"] = np.where(data["key"] == seed_row["key"].iloc[0], 1, 0)
-        data["time_signature"] = np.where(data["time_signature"] == seed_row["time_signature"].iloc[0], 1, 0)
+        seed_row = big_data.loc[big_data["song_title"] == inputs["Seed Song"]]
+        #print(len(seed_row))
+        if(len(seed_row) > 0):
+            seed_existence = True
+            data = pd.concat([data, seed_row], ignore_index=True)
+            data = data.drop_duplicates()
+            
+            artists.append(seed_row["artist_name"].iloc[0])
+            decades.append(seed_row["year"].iloc[0])
+            
+            data["key"] = np.where(data["key"] == seed_row["key"].iloc[0], 1, 0)
+            data["time_signature"] = np.where(data["time_signature"] == seed_row["time_signature"].iloc[0], 1, 0)
     #Default settings for no seed song
     else:
         data["key"] = 0
@@ -43,7 +45,7 @@ def cluster(seed_song = None, artist = None, year = None, length = 2000):
     
     #Sets up year feature
     if (inputs["Year"] != None):
-        decades.append(inputs["year"])
+        decades.append(inputs["Year"])
     
     decades = [int(np.floor(x/10.0)*10) for x in decades]
     
@@ -56,11 +58,12 @@ def cluster(seed_song = None, artist = None, year = None, length = 2000):
     data["tempo"] = data["tempo"]/data["tempo"].max()
     
     
-    #Runs CLustering Algorithm. Uses OPTICS istead of DBSCAN Due to Memory Issues
+    #Runs CLustering Algorithm. Uses OPTICS instead of DBSCAN Due to Memory Issues
     #print("Its Clusterin Time")
     t1 = time.time()
     
     data = data.dropna()
+    #print(data.columns)
     clustering = OPTICS(max_eps = 3, min_samples=10).fit(data.iloc[:,np.r_[2, 4:14]])
     
     t2 = time.time()
@@ -70,7 +73,7 @@ def cluster(seed_song = None, artist = None, year = None, length = 2000):
     
     #Identifies points in the same cluster as the seed song
     #If no seed song is selected then select smallest cluster
-    if (inputs["Seed Song"] != None):
+    if (inputs["Seed Song"] != None and seed_existence):
         chosen_label = data.loc[data["song_title"] == inputs["Seed Song"]]["Cluster"].iloc[0]
         eligible_songs = data.loc[data["Cluster"] == chosen_label]
     else:
@@ -85,10 +88,10 @@ def cluster(seed_song = None, artist = None, year = None, length = 2000):
     while (remaining_time > 0 and len(eligible_songs) > 0):
         eligible_songs = eligible_songs.sample(frac = 1).reset_index(drop=True)
         if (eligible_songs["duration"].iloc[0] <= remaining_time):
-            playlist.append([eligible_songs["song_title"].iloc[0], eligible_songs["artist_name"].iloc[0]])
+            playlist.append([eligible_songs["song_title"].iloc[0], eligible_songs["artist_name"].iloc[0], eligible_songs["duration"].iloc[0]])
             remaining_time -= eligible_songs["duration"].iloc[0]
             
         eligible_songs = eligible_songs.drop(0)
         
-    #print(playlist)
+    print(playlist)
     return playlist
